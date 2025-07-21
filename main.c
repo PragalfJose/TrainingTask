7fe7f02ca7aafec6cc4fdde5100f098657ac8108
PragalfJose <pragalf.jose@trenser.com>
2025-07-21 11:10:36 +0530

    Coding Standards
    Solving first coding satndars issue

--- main.c
+++ main.c
@@ -5,14 +5,16 @@
 
 #include "PrintFunction/PrintFunction.h"
 
+#define STRING_SIZE 64
+
 int main()
 {
     // variables decleared here
-    uint8_t ucInputString[64];
+    uint8_t ucInputString[STRING_SIZE];
 
     memset(ucInputString,0,sizeof(ucInputString));
     PrintString("Enter a string");
-    scanf("%[^\n]s",ucInputString);
+    scanf("%64[^\n]s",ucInputString);
 
     PrintString(ucInputString);
     return 0;
