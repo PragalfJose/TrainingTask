34e0b57dbdda21cc0498d888f5d2a5dabf820341
PragalfJose <pragalf.jose@trenser.com>
2025-07-21 18:40:50 +0530

    Coding Standards
    Solving coding satndard issues 7

--- Global/Global.h
+++ Global/Global.h
@@ -15,13 +15,13 @@
 //******************************* Include Files *******************************
 
 //******************************* Global Types ********************************
-typedef signed char     int8;
-typedef unsigned char   uint8;
-typedef short       int16;
-typedef unsigned short      uint16;
-typedef int     int32;
-typedef unsigned     uint32;
-typedef long long       int64;
+typedef signed char             int8;
+typedef unsigned char           uint8;
+typedef short                   int16;
+typedef unsigned short          uint16;
+typedef int                     int32;
+typedef unsigned                uint32;
+typedef long long               int64;
 typedef unsigned long long      uint64;
 
 //***************************** Global Constants ******************************
